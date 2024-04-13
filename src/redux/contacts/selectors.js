ba4ef958b830c94d3d58078d65991db82883ee68
Serhii Plis <serzh2577@gmail.com>
2024-04-13 21:22:54 +0300

    ss

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,12 +1,12 @@
 import { createSelector } from "@reduxjs/toolkit";
-import { selectNameFilter } from "../filters/selectors";
+import { selectFilters } from "../filters/selectors";
 
 export const selectContacts = (state) => state.contacts.items;
 export const selectLoading = (state) => state.contacts.loading;
 export const selectError = (state) => state.contacts.error;
 
 export const selectFilteredContacts = createSelector(
-  [selectContacts, selectNameFilter],
+  [selectContacts, selectFilters],
   (contacts, filter) => {
     return contacts.filter(
       (contact) =>
